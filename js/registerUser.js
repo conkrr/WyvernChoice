7c47f33be2d040ead8c99d89a536e2f27410ce74
justgaret23 <sheehan.garrett23@gmail.com>
2020-11-20 15:04:37 -0600

    Updated HTML/CSS files

---
+++ js/registerUser.js
@@ -0,0 +1,43 @@
+/*
+* Respond to a user's attempt to register
+*/
+
+function processRegisterResponse(username, password){
+    //Grab any div or span element
+    //manipulate contents dynamically via Javascript
+    console.log("Username: " + username);
+    console.log("Password: " + password);
+
+    let user = JSON.parse(username);
+    let pw = JSON.parse(password);
+
+}
+
+function handleRegisterClick(e){
+    var form = document.addForm;
+    var username = form.username.value;
+    var password = form.password.value;
+
+    var data = {};
+    data["Username: "] = username;
+    data["Password: "] = password;
+
+    var js = JSON.stringify(data);
+    console.log("JS: " + js);
+
+    //Send POST request
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", register_user_url, true);
+    xhr.send(js);
+
+    //This processes results and updates HTML as appropriat
+    xhr.onloadend = function(){
+        console.log("XHR: " + xhr);
+        console.log("XHR Request: " + xhr.request);
+
+        if(xhr.readyState == XMLHttpRequest.DONE){
+            console.log ("XHR response text:" + xhr.responseText);
+            processRegisterResponse
+        }
+    }
+}
