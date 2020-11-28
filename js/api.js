c30181a4b7fbf653f731ebd43355ec157839a712
shanestevenz <45518527+shanestevenz@users.noreply.github.com>
2020-11-28 18:38:52 -0500

    Update api.js
    Iteration#1 code

--- js/api.js
+++ js/api.js
@@ -2,19 +2,20 @@
 // be sure you change to accommodate your specific API Gateway entry point
 
 //Base url: This will be the url we generate in AWS
-let base_url;
-let admin_url;
+var base_url = "https://uyttzmj0g3.execute-api.us-east-1.amazonaws.com/testStage/";
+var admin_url = "https://uyttzmj0g3.execute-api.us-east-1.amazonaws.com/testStage/";
+
 
 //All unique urls generated for each request
-let choice_list_url = admin_url + "admin";                    //GET
-let delete_date_url = admin_url + "admin";                    //POST with {deleteData}
-let get_choice_url = base_url + "choice";                    //GET with {choiceID}
-let create_choice_url = base_url + "choice";                 //GET
-let finalize_choice_url = base_url + "finalChoice";          //POST
-let register_user_url = base_url + "register";               //POST
-let get_alternative_url = base_url + "alternative";          //GET with {alternativeID}
-let add_approval_url = base_url + "addapproval";             //POST
-let remove_approval_url = base_url + "removeapproval";       //POST
-let add_disapproval_url = base_url + "adddisapproval";       //POST
-let remove_disapproval_url = base_url + "removedisapproval"; //POST
-let create_feedback_url = base_url + "feedback";             //POST
+var choice_list_url = admin_url + "admin";                    //GET
+var delete_date_url = admin_url + "admin";                    //POST with {deleteData}
+var get_choice_url = base_url + "choice";                    //GET with {choiceID}
+var create_choice_url = base_url + "choice";                 //GET
+var finalize_choice_url = base_url + "finalChoice";          //POST
+var register_user_url = base_url + "register";               //POST
+var get_alternative_url = base_url + "alternative";          //GET with {alternativeID}
+var add_approval_url = base_url + "addapproval";             //POST
+var remove_approval_url = base_url + "removeapproval";       //POST
+var add_disapproval_url = base_url + "adddisapproval";       //POST
+var remove_disapproval_url = base_url + "removedisapproval"; //POST
+var create_feedback_url = base_url + "feedback";             //POST
