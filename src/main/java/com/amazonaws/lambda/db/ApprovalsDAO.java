4469d86a37963e0f6c23d5282259cb3870b93730
Jason Conklin <jaconklin@wpi.edu>
2020-12-01 02:02:50 -0500

    working on generic tests for the DAOs

--- src/main/java/com/amazonaws/lambda/db/ApprovalsDAO.java
+++ src/main/java/com/amazonaws/lambda/db/ApprovalsDAO.java
@@ -8,17 +8,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
 import com.amazonaws.lambda.demo.model.Approval;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
+//import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
 public class ApprovalsDAO implements DataAccess<List<Approval>>{
 
 	
 	private Connection connection;
 	private static final String tableName = "Approvals";
-	private final LambdaLogger logger;
+//	private final LambdaLogger logger;
 	
-    public ApprovalsDAO(LambdaLogger logger) {
-    	this.logger = logger;
+    public ApprovalsDAO(/*LambdaLogger logger*/) {
+//    	this.logger = logger;
     	try  {
     		connection = DatabaseUtil.connect();
     	} catch (Exception e) {
@@ -28,7 +28,7 @@ public class ApprovalsDAO implements DataAccess<List<Approval>>{
 
 	@Override
 	public List<Approval> get(String uniqueId) throws Exception {
-		logger.log("ApprovalsDAO::get() -- Begin");
+		//logger.log("ApprovalsDAO::get() -- Begin");
     	List<Approval> list;  	
         try {
             PreparedStatement ps = connection.prepareStatement("SELECT Users.userID, Users.name, Approvals.status FROM Users JOIN Alternatives ON Users.choiceID = Alternatives.choiceID JOIN Approvals ON Approvals.alternativeID = Alternatives.alternativeID AND Approvals.userID = Users.userID JOIN (SELECT Approvals.userID, MAX(Approvals.timestamp) AS timestamp FROM Approvals WHERE Approvals.alternativeID = ? GROUP BY Approvals.userID) Maxtimes ON Users.userID = Maxtimes.userID AND Approvals.timestamp = Maxtimes.timestamp WHERE Approvals.alternativeID = ?");           
@@ -38,7 +38,7 @@ public class ApprovalsDAO implements DataAccess<List<Approval>>{
             list = generate(resultSet);
             resultSet.close();
             ps.close();
-            logger.log("ApprovalsDAO::get() -- End");
+            //logger.log("ApprovalsDAO::get() -- End");
             return list;
         } catch (Exception e) {
         	e.printStackTrace();
@@ -48,7 +48,7 @@ public class ApprovalsDAO implements DataAccess<List<Approval>>{
 
 	@Override
 	public int delete(String uniqueId) throws Exception {
-		logger.log("ApprovalsDAO::delete() -- Begin");
+		//logger.log("ApprovalsDAO::delete() -- Begin");
 		int rowsAffected = 0;
 		
 		try {
@@ -56,7 +56,7 @@ public class ApprovalsDAO implements DataAccess<List<Approval>>{
             ps.setString(1, uniqueId);
             rowsAffected = ps.executeUpdate();
             ps.close();       
-            logger.log("ApprovalsDAO::delete() -- End");
+            //logger.log("ApprovalsDAO::delete() -- End");
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new Exception("Exception in ApprovalsDAO::delete(): " + e.getMessage());
@@ -66,7 +66,7 @@ public class ApprovalsDAO implements DataAccess<List<Approval>>{
 
 	@Override
 	public boolean insert(List<Approval> t) throws Exception {
-		logger.log("ApprovalsDAO::insert -- Begin");
+		//logger.log("ApprovalsDAO::insert -- Begin");
 		try {
 			boolean alreadyExists = get(t.get(0).getAlternativeId()) != null;
 			if (!alreadyExists) {
@@ -81,7 +81,7 @@ public class ApprovalsDAO implements DataAccess<List<Approval>>{
 					ps.execute();
 				} 
 			}
-			logger.log("ApprovalsDAO::insert() -- End");
+			//logger.log("ApprovalsDAO::insert() -- End");
 			return alreadyExists;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -91,7 +91,7 @@ public class ApprovalsDAO implements DataAccess<List<Approval>>{
 
 	@Override
 	public List<Approval> generate(ResultSet resultSet) throws Exception {
-		logger.log("ApprovalsDAO::generate() -- Begin");
+		//logger.log("ApprovalsDAO::generate() -- Begin");
         
     	ArrayList<Approval> approvals = new ArrayList<Approval>();
         try {
@@ -111,7 +111,7 @@ public class ApprovalsDAO implements DataAccess<List<Approval>>{
         	e.printStackTrace();
             throw new Exception("Exception in ApprovalsDAO::generate(): " + e.getMessage());
         }
-        logger.log("ApprovalsDAO::generate() -- End");
+        //logger.log("ApprovalsDAO::generate() -- End");
         return approvals;
 	}
 
