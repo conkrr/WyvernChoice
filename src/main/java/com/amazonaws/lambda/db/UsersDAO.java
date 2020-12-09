3abd7ec5f18749219649e8f7754054e2c4a4937d
Jason Conklin <jaconklin@wpi.edu>
2020-12-09 14:28:28 -0500

    fixed typo with wrong table name

--- src/main/java/com/amazonaws/lambda/db/UsersDAO.java
+++ src/main/java/com/amazonaws/lambda/db/UsersDAO.java
@@ -69,7 +69,7 @@ java.sql.Connection connection;
              int currentUsers;
              int maxUsers;
              
-             PreparedStatement ps = connection.prepareStatement("SELECT * FROM " + tableName + " WHERE choiceID=?;");
+             PreparedStatement ps = connection.prepareStatement("SELECT * FROM Choices WHERE choiceID=?;");
             
              ps.setString(1,  choiceId);
             
