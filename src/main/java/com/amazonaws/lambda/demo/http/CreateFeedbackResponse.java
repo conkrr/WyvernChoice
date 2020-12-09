6f1acb577a94054e7b7a54da5d3c4fafef0e90f7
justgaret23 <sheehan.garrett23@gmail.com>
2020-12-08 18:35:15 -0600

    oh wait response

--- src/main/java/com/amazonaws/lambda/demo/http/CreateFeedbackResponse.java
+++ src/main/java/com/amazonaws/lambda/demo/http/CreateFeedbackResponse.java
@@ -20,7 +20,7 @@ public class CreateFeedbackResponse {
 	
 	public String toString() {
 		if(statusCode == 200) {
-			return "{ \"feedbackUser\": \"" + feedback.user + "\", \"feedbackText\": \"" + feedback.text + "\", \"feedbackTimestamp\": \"" + feedback.timestamp + "\", \"statusCode\": \"" + statusCode +"\" }";
+			return "{ \"feedbackUser\": \"" + feedback.userID + "\", \"feedbackText\": \"" + feedback.text + "\", \"feedbackTimestamp\": \"" + feedback.timestamp + "\", \"statusCode\": \"" + statusCode +"\" }";
 		} else {
 			return "{ \"statusCode\": \"" + statusCode + "\", \"error\": \"" + error + "\" }";
 		}
