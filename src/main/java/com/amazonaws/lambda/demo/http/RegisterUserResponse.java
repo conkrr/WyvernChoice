270ed05882abb53f3940b35a72a31ecb0973b542
Jason Conklin <jaconklin@wpi.edu>
2020-12-09 22:03:35 -0500

    fixed mismatched name

--- src/main/java/com/amazonaws/lambda/demo/http/RegisterUserResponse.java
+++ src/main/java/com/amazonaws/lambda/demo/http/RegisterUserResponse.java
@@ -4,28 +4,28 @@ public class RegisterUserResponse {
 
 	public UserGsonCompatible userGson;
 	
-	public int statusCode;
+	public int status;
 	public String error;
 	
 	public RegisterUserResponse(UserGsonCompatible u) {
 		this.userGson = u;
-		this.statusCode = 200;
+		this.status = 200;
 	}
 	
 	public RegisterUserResponse(int statusCode, String errorMessage) {
-		this.statusCode = statusCode;
+		this.status = statusCode;
 		this.error = errorMessage;
 	}
 	
 	public RegisterUserResponse () {
-		this.statusCode = 200;
+		this.status = 200;
 	}
 	
 	public String toString() {
-		if(statusCode == 200) {
-			return "{ \"username\": \"" + userGson.username + "\", \"password\": \"" + userGson.password + "\", \"choiceID\": \"" + userGson.choiceID + "\", \"statusCode\": \"" + statusCode +"\" }";
+		if(status == 200) {
+			return "{ \"username\": \"" + userGson.username + "\", \"password\": \"" + userGson.password + "\", \"choiceID\": \"" + userGson.choiceID + "\", \"statusCode\": \"" + status +"\" }";
 		} else {
-			return "{ \"statusCode\": \"" + statusCode + "\", \"error\": \"" + error + "\" }";
+			return "{ \"statusCode\": \"" + status + "\", \"error\": \"" + error + "\" }";
 		}
 	}
 	
