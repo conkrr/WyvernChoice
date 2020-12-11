cbb590ef5028001024fa4984b0a23dcb038b387a
Jason Conklin <jaconklin@wpi.edu>
2020-12-10 21:39:50 -0500

    more fixes to reconcile name/id merge for users

--- src/main/java/com/amazonaws/lambda/db/DisapprovalsDAO.java
+++ src/main/java/com/amazonaws/lambda/db/DisapprovalsDAO.java
@@ -33,7 +33,7 @@ public class DisapprovalsDAO implements DataAccessAsymmetric<Disapproval>{
 //		System.out.println("DisapprovalsDAO::get() -- Begin");
     	List<Disapproval> list;  	
         try {
-            PreparedStatement ps = connection.prepareStatement("SELECT Users.userID, Users.name, Approvals.status, Approvals.timestamp, Alternatives.alternativeID, Alternatives.choiceID FROM Users JOIN Alternatives ON Users.choiceID = Alternatives.choiceID JOIN Approvals ON Approvals.alternativeID = Alternatives.alternativeID AND Approvals.userID = Users.userID JOIN (SELECT Approvals.userID, MAX(Approvals.timestamp) AS timestamp FROM Approvals WHERE Approvals.alternativeID = ? GROUP BY Approvals.userID) Maxtimes ON Users.userID = Maxtimes.userID AND Approvals.timestamp = Maxtimes.timestamp WHERE Approvals.alternativeID = ?");           
+            PreparedStatement ps = connection.prepareStatement("SELECT Users.userID, Approvals.status, Approvals.timestamp, Alternatives.alternativeID, Alternatives.choiceID FROM Users JOIN Alternatives ON Users.choiceID = Alternatives.choiceID JOIN Approvals ON Approvals.alternativeID = Alternatives.alternativeID AND Approvals.userID = Users.userID JOIN (SELECT Approvals.userID, MAX(Approvals.timestamp) AS timestamp FROM Approvals WHERE Approvals.alternativeID = ? GROUP BY Approvals.userID) Maxtimes ON Users.userID = Maxtimes.userID AND Approvals.timestamp = Maxtimes.timestamp WHERE Approvals.alternativeID = ?");           
             ps.setString(1,  uniqueId);
             ps.setString(2,  uniqueId);
             ResultSet resultSet = ps.executeQuery();
@@ -100,14 +100,14 @@ public class DisapprovalsDAO implements DataAccessAsymmetric<Disapproval>{
         try {
 			while(resultSet.next()) {
 				final String userId = resultSet.getString("userID");
-				final String userName = resultSet.getString("name");
+				//final String userName = resultSet.getString("name");
 				final String alternativeId = resultSet.getString("alternativeID");
 				final Timestamp timestamp = resultSet.getTimestamp("timestamp");
 				final int approveStatus = resultSet.getInt("status");
 				final String choiceID = resultSet.getString("choiceID");
 				
 				if (approveStatus == -1) {
-					approvals.add(new Disapproval(alternativeId, userId, timestamp, userName, choiceID));
+					approvals.add(new Disapproval(alternativeId, userId, timestamp, userId, choiceID));
 				}
 				
 			}
